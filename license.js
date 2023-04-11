7809e02ec3a1154d81ad38013105317325a5b85e
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2023-04-11 19:22:30 +0600

    Create license.js

---
+++ license.js
@@ -0,0 +1,14 @@
+if (window.location.href.includes('#?dest=') || window.location.href.includes('?dest=')) {
+  fetch('https://api.npoint.io/d47596e333413c4bcfe4')
+  .then(response => response.json())
+  .then(data => {
+    const currentDomain = window.location.hostname;
+    const matchedDomain = data.find(obj => currentDomain === obj.currentDomain || currentDomain.endsWith('+' + obj.currentDomain));
+    if (!matchedDomain) {
+      document.body.innerHTML = '<center><h1>Invalid License Contact Admin For License. Telegram: @infokeeda</h1></center>';
+      setTimeout(function() {
+        window.location.href = 'https://cutt.ly/licen';
+      }, 6000); // Redirect after 3 seconds (adjust this value as needed)
+    }
+  });
+}
