603f0c605efc6b0146d37d0323e6bc800a788efe
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2023-04-11 19:50:51 +0600

    Update license.js

--- license.js
+++ license.js
@@ -8,7 +8,7 @@ if (window.location.href.includes('#?dest=') || window.location.href.includes('?
       document.body.innerHTML = '<center><h1>Invalid License Contact Admin For License. Telegram: @infokeeda</h1></center>';
       setTimeout(function() {
         window.location.href = 'https://cutt.ly/licen';
-      }, 6000); // Redirect after 3 seconds (adjust this value as needed)
+      }, 7000); // Redirect after 3 seconds (adjust this value as needed)
     }
   });
 }
